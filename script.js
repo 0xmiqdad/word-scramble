8431adae213a3c62a6084337d419feceee0a61dc
Miqdad O. Adams <77921798+miqdadtez@users.noreply.github.com>
2022-08-25 18:50:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const wordText = document.querySelector(".word")
+
+const initGame = () => {
+   let randomObj = words[Math.floor(Math.random()* words.length)];
+   let wordArray = randomObj.word.split("");
+   for (let i = wordArray.length -1; i > 0; i--){
+    let j = Math.floor(Math.random()* (i+1));
+    [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]];
+   }
+    wordText.innerText =
+   console.log(wordArray, randomObj.word);
+}
+
+initGame();
---
+++ word.js
@@ -0,0 +1,57 @@
+const words = [
+
+    {
+        word: "nawa oh",
+        hint: "giving up"
+   },
+
+   {
+        word: "nawa oh",
+        hint: "giving up"
+   },
+
+   {
+        word: "nawa oh",
+        hint: "giving up"
+   },
+
+   {
+       word: "nawa oh",
+       hint: "giving up"
+   },
+
+   {
+    word: "nawa oh",
+    hint: "giving up"
+   },
+
+   {
+    word: "nawa oh",
+    hint: "giving up"
+   },
+   
+   {
+    word: "nawa oh",
+    hint: "giving up"
+   },
+   
+   {
+    word: "nawa oh",
+    hint: "giving up"
+   },
+   
+   {
+    word: "nawa oh",
+    hint: "giving up"
+   },
+   
+   {
+    word: "nawa oh",
+    hint: "giving up"
+   },
+   
+   {
+    word: "nawa oh",
+    hint: "giving up"
+   },
+]
